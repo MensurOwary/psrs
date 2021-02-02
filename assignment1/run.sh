054447d265fe578e5fd789ee1ace6b67a734f88c
MensurOwary <mensurowary@gmail.com>
2021-02-02 05:42:10 +0000

    Added data

---
+++ assignment1/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+KEYS=( 16000000 32000000 64000000 128000000 256000000 512000000 )
+
+mkdir -p data
+
+make compile
+
+for thread_count in 2 3 4 5 6
+do
+	for size in "${KEYS[@]}"
+	do
+		for time in {1..5}
+		do
+			echo "Running ${thread_count} : ${size} - $time time"
+			./main $size $thread_count > ./data/out"$thread_count-$size-$time".txt
+		done
+	done
+done
