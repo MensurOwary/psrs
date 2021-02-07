134b2baa2d1aa1a353cc46f618078393316b4469
MensurOwary <mensurowary@gmail.com>
2021-02-07 05:32:54 +0000

    Update

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -6,11 +6,11 @@ mkdir -p data2
 
 make compile
 
-for thread_count in 2 3 4 5 6
+for thread_count in 7 8
 do
 	for size in "${KEYS[@]}"
 	do
-		for time in {1..5}
+		for time in {1..1}
 		do
 			echo "Running ${thread_count} : ${size} - $time time"
 			./main.o $size $thread_count > ./data2/out"$thread_count-$size-$time".txt
