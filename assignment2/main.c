7221c24afaa7954c0b403609ff9c2507b8808d46
MensurOwary <mensurowary@gmail.com>
2021-03-03 08:17:02 +0000

    Revert "Sync"
    This reverts commit 7ba3d84eebdff08f40843581d1e09a7c9eb76e16.

--- assignment2/main.c
+++ assignment2/main.c
@@ -3,14 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ROOT 0
-#define MASTER if (rank == ROOT)
-#define ISIZE sizeof(int)
-
 int* generateArrayDefault() {
 	int arr[36] = {16, 2, 17, 24, 33, 28, 30, 1, 0, 27, 9, 25, 34, 23, 19, 18, 11, 7, 21, 13, 8, 35, 12, 29, 6, 3, 4, 14, 22, 15, 32, 10, 26, 31, 20, 5};
 
-	int* randoms = malloc(ISIZE * 36);
+	int* randoms = malloc(sizeof(int) * 36);
 	for (int i = 0; i < 36; i++) {
 		randoms[i] = arr[i];
 	}
@@ -22,15 +18,6 @@ int cmpfunc (const void * a, const void * b) { return ( *(int *) a - *(int*)b );
 void printArray(int* a, int size) {
 	for (int i = 0; i < size; i++) printf("%d ", a[i]);
 	printf("\n");
-}
-
-void send(int* arr, int size, int dest) {
-	MPI_Send(arr, size, MPI_INT, dest, 0, MPI_COMM_WORLD);
-}
-
-void recv(int* arr, int size, int source) {
-	printf("%d\n", size);
-	MPI_Recv(arr, size, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 }
 
 int main() {
@@ -54,42 +41,42 @@ int main() {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
 	// Phase 0: Data distribution
-	int* partition = malloc(ISIZE * perProcessor);
-	MASTER {
-		memcpy(partition, DATA, ISIZE * perProcessor);
+	int* partition = malloc(sizeof(int) * perProcessor);
+	if (rank == 0) {
+		memcpy(partition, DATA, sizeof(int) * perProcessor);
 		for (int i = 1; i < T; i++) {
 			int* start = DATA + (i * perProcessor);
-			send(start, perProcessor, i);
+			MPI_Send(start, perProcessor, MPI_INT, i, 0, MPI_COMM_WORLD);
 		}
 	} else {
-		recv(partition, perProcessor, ROOT);
+		MPI_Recv(partition, perProcessor, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
 	// Phase 1: Sorting local data
-	qsort(partition, perProcessor, ISIZE, cmpfunc);
+	qsort(partition, perProcessor, sizeof(int), cmpfunc);
 	
 	// Phase 1: Regular sampling
 	/* regular sampling */
 	int ix = 0;
-	int* localRegularSamples = malloc(ISIZE * T);
+	int* localRegularSamples = malloc(sizeof(int) * T);
 	for (int i = 0; i < T; i++) {
 		localRegularSamples[ix++] = partition[i * W];
 	}
 	// Phase 2: Sending samples to master
-	int* regularSamples = malloc(ISIZE * T * T);
-	MASTER {
+	int* regularSamples = malloc(sizeof(int) * T * T);
+	if (rank == 0) {
 		for (int i = 1; i < T; i++) {
 			int* start = regularSamples + (i * T);
-			recv(start, T, i);
+			MPI_Recv(start, T, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		}
-		memcpy(regularSamples, localRegularSamples, ISIZE * T); 
+		memcpy(regularSamples, localRegularSamples, sizeof(int) * T); 
 	} else {
-		send(localRegularSamples, T, ROOT); 
+		MPI_Send(localRegularSamples, T, MPI_INT, 0, 0, MPI_COMM_WORLD); 
 	}
 	printf("%d) Phase 2 sending samples done\n", rank);
 	// Phase 2: Sorting and picking pivots
-	int* pivots = malloc(ISIZE * (T - 1));
-	MASTER {
-		qsort(regularSamples, T * T, ISIZE, cmpfunc);
+	int* pivots = malloc(sizeof(int) * (T - 1));
+	if (rank == 0) {
+		qsort(regularSamples, T * T, sizeof(int), cmpfunc);
 		for (int i = 1, ix = 0; i < T; i++) {
 			int pos = T * i + RO - 1;
 			pivots[ix++] = regularSamples[pos];
