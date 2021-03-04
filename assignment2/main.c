d902acb0f4206040e4bfea159e145391ece98ba3
MensurOwary <mensurowary@gmail.com>
2021-03-04 01:25:29 +0000

    Sync

--- assignment2/main.c
+++ assignment2/main.c
@@ -26,7 +26,13 @@ int main() {
 	int SIZE = 36;
 
 	MPI_Init(NULL, NULL);
+
+	// Get the name of the processor
+  	char processor_name[MPI_MAX_PROCESSOR_NAME];
+  	int name_len;
+  	MPI_Get_processor_name(processor_name, &name_len);
 	
+
 	// how many processors are available
 	int T;
 	MPI_Comm_size(MPI_COMM_WORLD, &T);
@@ -40,6 +46,7 @@ int main() {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
+	printf("%s with rank %d is up and ready to go!", processor_name, rank);
 	// Phase 0: Data distribution
 	int* partition = malloc(sizeof(int) * perProcessor);
 	if (rank == 0) {
