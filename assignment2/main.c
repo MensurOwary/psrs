983adc08267b51f6b4caf1d75c1204454347faed
MensurOwary <mensurowary@gmail.com>
2021-03-04 02:42:11 +0000

    Sync

--- assignment2/main.c
+++ assignment2/main.c
@@ -148,6 +148,7 @@ int main() {
 				printf("%d: Waiting to receive len(%d) from %d\n", rank, lengths[j], j); 
 				int* keysReceived = malloc(sizeof(int) * lengths[j]);
                                 MPI_Recv(keysReceived, lengths[j], MPI_INT, j, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				printf("%d: Finished waiting for len(%d) from %d\n", rank, lengths[j], j);
                                 sum = 0;
         			for (int k = 0; k < j; j++) {
                 			sum += lengths[j];
