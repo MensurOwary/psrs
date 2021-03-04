e13ee39f3f3135dd1074b15915ca599c9324a6f1
MensurOwary <mensurowary@gmail.com>
2021-03-04 06:13:14 +0000

    Sync

--- assignment2/main.c
+++ assignment2/main.c
@@ -196,7 +196,7 @@ int main() {
 		for (int i = 0; i <= T; i++) {
 			if (indices[i] != indices[i+1]) {
 				if (obtainedKeys[indices[i]] < min) {
-					min = obtainedKeys[ix];
+					min = obtainedKeys[indices[i]];
 					pos = i;
 				}
 			}
