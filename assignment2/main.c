6d68611daf75f0dc00eaa73adb887be38c128d73
MensurOwary <mensurowary@gmail.com>
2021-03-04 08:10:22 +0000

    Sync

--- assignment2/main.c
+++ assignment2/main.c
@@ -7,12 +7,12 @@
 #define MASTER if (rank == 0)
 #define SLAVE else
 
-int* generateArrayDefault() {
-	int arr[36] = {16, 2, 17, 24, 33, 28, 30, 1, 0, 27, 9, 25, 34, 23, 19, 18, 11, 7, 21, 13, 8, 35, 12, 29, 6, 3, 4, 14, 22, 15, 32, 10, 26, 31, 20, 5};
-
-	int* randoms = malloc(sizeof(int) * 36);
-	for (int i = 0; i < 36; i++) {
-		randoms[i] = arr[i];
+int* generateArrayDefault(int size) {
+	// int arr[36] = {16, 2, 17, 24, 33, 28, 30, 1, 0, 27, 9, 25, 34, 23, 19, 18, 11, 7, 21, 13, 8, 35, 12, 29, 6, 3, 4, 14, 22, 15, 32, 10, 26, 31, 20, 5};
+	srandom(15);
+	int* randoms = malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++) {
+		randoms[i] = (int) random(); // arr[i];
 	}
 	return randoms;
 }
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]) {
 	// Phase 0: Data distribution
 	int* partition = malloc(sizeof(int) * perProcessor);
 	MASTER {
-		int* DATA = generateArrayDefault();
+		int* DATA = generateArrayDefault(SIZE);
 		memcpy(partition, DATA, sizeof(int) * perProcessor);
 		for (int i = 1; i < T; i++) {
 			int* start = DATA + (i * perProcessor);
