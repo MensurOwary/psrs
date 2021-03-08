36296ab97f8957431fc39f58768570ad87ae7d82
MensurOwary <mensurowary@gmail.com>
2021-03-08 09:06:05 +0000

    Sync

--- assignment2/main.c
+++ assignment2/main.c
@@ -218,7 +218,7 @@ int main(int argc, char *argv[]) {
 		lengths = NULL;
 	}
 	
-	MPI_Gatherv(&mergedArray, obtainedKeysSize, MPI_INT, FINAL, lengths, disp, MPI_INT, ROOT, MPI_COMM_WORLD);
+	MPI_Gatherv(mergedArray, obtainedKeysSize, MPI_INT, FINAL, lengths, disp, MPI_INT, ROOT, MPI_COMM_WORLD);
 	MASTER {
 		isSorted(FINAL, SIZE);
 	}
