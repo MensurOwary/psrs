e8547c0839c55211cd8887d20a156c0028e5a07c
MensurOwary <mensurowary@gmail.com>
2021-03-14 18:47:27 +0000

    Sync

--- assignment2/main.c
+++ assignment2/main.c
@@ -200,12 +200,12 @@ long int endTiming(struct timeval* start) {
 	return diff;
 }
 
-void measureTime(void (*fun)(), char* processorName, char* title, int shouldLog) {
+void measureTime(void (*fun)(), char* processorName, char* title, int rank, int shouldLog) {
 	if (shouldLog) {
 		struct timeval* start = getTime();
 		fun();
 		long int time = endTiming(start);
-		printf("[%s] %s took %ld ms\n", processorName, title, time);
+		printf("[%s:%d] %s took %ld ms\n", processorName, rank, title, time);
 	} else {
 		fun();
 	}
@@ -229,19 +229,19 @@ int main(int argc, char *argv[]) {
     	MPI_Get_processor_name(processor_name, &name_len);
 
 	// Phase 0: Data distribution
-	measureTime(phase_0, processor_name, "Phase 0", rank == 0);
+	measureTime(phase_0, processor_name, "Phase 0", rank, rank == 0);
 	struct timeval* start = getTime();
 	// PHASE 1
-	measureTime(phase_1, processor_name, "Phase 1", 1);
+	measureTime(phase_1, processor_name, "Phase 1", rank, 1);
 	// PHASE 2
-	measureTime(phase_2, processor_name, "Phase 2", rank == 0);
+	measureTime(phase_2, processor_name, "Phase 2", rank, rank == 0);
 	// PHASE 3
-	measureTime(phase_3, processor_name, "Phase 3", 1);
+	measureTime(phase_3, processor_name, "Phase 3", rank, 1);
 	// PHASE 4
-	measureTime(phase_4, processor_name, "Phase 4", 1);
+	measureTime(phase_4, processor_name, "Phase 4", rank, 1);
 	long int time = endTiming(start);
 	// PHASE Merge	
-	measureTime(phase_merge, processor_name, "Phase Merge", rank == 0);
+	measureTime(phase_merge, processor_name, "Phase Merge", rank, rank == 0);
 	
 	MASTER printf("Complete execution took: %ld ms\n", time);
 	
